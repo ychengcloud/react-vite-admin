8fb86d2f5909e4d91fc2234d93b123b7c772c356
decker502 <decker502@qq.com>
2021-03-31 14:43:28 +0800

    use protable

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -2,27 +2,15 @@ import { LocaleFormatter, useLocale } from "@/locales";
 import { FooterToolbar, PageContainer } from "@ant-design/pro-layout";
 import type { ProColumns, ActionType } from "@ant-design/pro-table";
 import ProTable from "@ant-design/pro-table";
-import { Button, Card, message, Modal, Space, Table } from "antd";
+import { Button, message, Modal } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 
 import React, { useEffect, useRef, useState } from "react";
 import { findDOMNode } from "react-dom";
 import OperationModal from "./components/OperationModal";
-import {
-  useAddProject,
-  useBatchDeleteProject,
-  useGetProjects,
-  useUpdateProject,
-} from "@/api";
-import {
-  ProFormDatePicker,
-  ProFormDateRangePicker,
-  ProFormSelect,
-  ProFormText,
-  QueryFilter,
-} from "@ant-design/pro-form";
+import { useAddProject, useBatchDeleteProject, useGetProjects, useUpdateProject } from "@/api";
 
-const TableList = () => {
+const TableList= () => {
   const { formatMessage } = useLocale();
 
   const addBtn = useRef(null);
@@ -45,10 +33,7 @@ const TableList = () => {
   const actionRef = useRef<ActionType>();
   const [selectedRowsState, setSelectedRows] = useState<API.Project[]>([]);
 
-  const { data, error, isLoading, refetch } = useGetProjects(
-    pagination,
-    filters
-  );
+  const { data, error, isLoading, refetch } = useGetProjects(pagination, filters);
 
   const { mutateAsync } = useAddProject();
   const { mutateAsync: update } = useUpdateProject();
@@ -147,126 +132,123 @@ const TableList = () => {
       return false;
     }
   };
-  const columns = [
+
+  const columns: ProColumns<API.Project>[] = [
     {
       title: formatMessage({ id: "app.project.name" }),
       dataIndex: "name",
+      tip: "项目名称是唯一的 key",
+      sorter: true,
+      render: (dom, entity) => {
+        return (
+          <a
+            onClick={() => {
+              setCurrent(entity);
+              setShowDetail(true);
+            }}
+          >
+            {dom}
+          </a>
+        );
+      },
     },
     {
       title: formatMessage({ id: "app.project.description" }),
       dataIndex: "description",
+      valueType: "textarea",
+      sorter: true,
+    },
+    {
+      title: formatMessage({ id: "gloabal.tips.operation" }),
+      dataIndex: "option",
+      valueType: "option",
+      render: (_, record) => [
+        <a
+          key="edit"
+          onClick={(e) => {
+            e.preventDefault();
+            showEditModal(record);
+          }}
+        >
+          {formatMessage({ id: "gloabal.tips.modify" })}
+        </a>,
+        <a
+          key="delete"
+          onClick={(e) => {
+            e.preventDefault();
+            Modal.confirm({
+              title: "删除項目",
+              content: "确定删除该項目吗？",
+              okText: "确认",
+              cancelText: "取消",
+              onOk: async () => {
+                await handleRemove([{ ...record }]);
+                setSelectedRows([]);
+                refetch();
+              },
+            });
+          }}
+        >
+          {formatMessage({ id: "gloabal.tips.delete" })}
+        </a>,
+      ],
     },
   ];
-  // const columns: ProColumns<API.Project>[] = [
-  //   {
-  //     title: formatMessage({ id: "app.project.name" }),
-  //     dataIndex: "name",
-  //     tip: "项目名称是唯一的 key",
-  //     sorter: true,
-  //     render: (dom, entity) => {
-  //       return (
-  //         <a
-  //           onClick={() => {
-  //             setCurrent(entity);
-  //             setShowDetail(true);
-  //           }}
-  //         >
-  //           {dom}
-  //         </a>
-  //       );
-  //     },
-  //   },
-  //   {
-  //     title: formatMessage({ id: "app.project.description" }),
-  //     dataIndex: "description",
-  //     valueType: "textarea",
-  //     sorter: true,
-  //   },
-  //   {
-  //     title: formatMessage({ id: "gloabal.tips.operation" }),
-  //     dataIndex: "option",
-  //     valueType: "option",
-  //     render: (_, record) => [
-  //       <a
-  //         key="edit"
-  //         onClick={(e) => {
-  //           e.preventDefault();
-  //           showEditModal(record);
-  //         }}
-  //       >
-  //         {formatMessage({ id: "gloabal.tips.modify" })}
-  //       </a>,
-  //       <a
-  //         key="delete"
-  //         onClick={(e) => {
-  //           e.preventDefault();
-  //           Modal.confirm({
-  //             title: "删除項目",
-  //             content: "确定删除该項目吗？",
-  //             okText: "确认",
-  //             cancelText: "取消",
-  //             onOk: async () => {
-  //               await handleRemove([{ ...record }]);
-  //               setSelectedRows([]);
-  //               refetch();
-  //             },
-  //           });
-  //         }}
-  //       >
-  //         {formatMessage({ id: "gloabal.tips.delete" })}
-  //       </a>,
-  //     ],
-  //   },
-  // ];
 
   return (
     <PageContainer>
-      <div className="table-filter">
-        <QueryFilter<API.Project[]>
-          onFinish={async (values) => {
-            console.log(values);
-            setFilters(values);
-          }}
-        >
-          <ProFormText
-            name="name"
-            label={formatMessage({ id: "app.project.name" })}
-            
-            rules={[{ required: true }]}
-          />
-          <ProFormText name="description" label={formatMessage({ id: "app.project.description" })} />
-        </QueryFilter>
-      </div>
-
-      <Card style={{ marginTop: 16 }}>
-        <div style={{ marginBottom: 16, display: 'flex', justifyContent: "flex-end" }}>
-          <Space>
-            <Button type="primary" key="primary" onClick={showModal}>
-              <PlusOutlined /> <LocaleFormatter id="gloabal.tips.create" />
-            </Button>
-          </Space>
-        </div>
-        <Table<API.Project>
-          rowKey="id"
-          dataSource={projects}
-          columns={columns}
-          rowSelection={{
-            onChange: (_, selectedRows) => {
-              setSelectedRows(selectedRows);
-            },
-          }}
-        />
-      </Card>
-
+      <ProTable<API.Project>
+        headerTitle={formatMessage({
+                id: 'app.project.title',
+                defaultMessage: '项目管理',
+              })}
+        actionRef={actionRef}
+        rowKey="id"
+        options={{reload: false}}
+        toolBarRender={() => [
+          <Button type="primary" key="primary" onClick={showModal}>
+            <PlusOutlined /> <LocaleFormatter id="gloabal.tips.create" />
+          </Button>,
+        ]}
+        request={undefined}
+        dataSource={projects}
+        columns={columns}
+        rowSelection={{
+          onChange: (_, selectedRows) => {
+            setSelectedRows(selectedRows);
+          },
+        }}
+        search={{
+          defaultCollapsed: false,
+          optionRender: ({ searchText, resetText }, { form }) => [
+            <Button
+              key="search"
+              type="primary"
+              onClick={() => {
+                // form?.submit();
+                console.log("search submit");
+                setFilters(form?.getFieldsValue());
+              }}
+            >
+              {searchText}
+            </Button>,
+            <Button
+              key="reset"
+              onClick={() => {
+                form?.resetFields();
+              }}
+            >
+              {resetText}
+            </Button>,
+          ],
+        }}
+      />
       {selectedRowsState?.length > 0 && (
         <FooterToolbar
           extra={
             <div>
-              <LocaleFormatter
-                id="app.project.chosen"
-                defaultMessage="已选择"
-              />{" "}
-              <a style={{ fontWeight: 600 }}>{selectedRowsState.length}</a>{" "}
+              <LocaleFormatter id="app.project.chosen" defaultMessage="已选择" />{' '}
+              <a style={{ fontWeight: 600 }}>{selectedRowsState.length}</a>{' '}
               <LocaleFormatter id="app.project.item" defaultMessage="项" />
             </div>
           }
@@ -275,10 +257,11 @@ const TableList = () => {
             onClick={async () => {
               await handleRemove(selectedRowsState);
               setSelectedRows([]);
-              refetch();
+                  refetch();
+
             }}
           >
-            <LocaleFormatter id="app.project.batchDeletion" />
+            <LocaleFormatter id="app.project.batchDeletion"  />
           </Button>
         </FooterToolbar>
       )}
