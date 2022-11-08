44a494d82c7b98f144ef6b75c867464a237ee672
He Xinpeng <hexinpeng1130@gmail.com>
2022-11-08 17:45:15 +0800

    fix: routes pravateRoute -> privateRoute

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { Route } from 'react-router-dom';
 import { RouteProps } from 'react-router';
-import PrivateRoute from './pravateRoute';
+import PrivateRoute from './privateRoute';
 import { useIntl } from 'react-intl';
 
 export interface WrapperRouteProps extends RouteProps {
